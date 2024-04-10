8d7ee9a7a8bedc4bfe5bb2d0121ea356c033f9b4
Pedro Costa <147849952+PCCP1234@users.noreply.github.com>
2024-04-10 17:43:31 +0100

    Update trivia-service.js

--- Trivia_Quiz/js/app/services/trivia-service.js
+++ Trivia_Quiz/js/app/services/trivia-service.js
@@ -6,27 +6,27 @@ define(["views/menu-view"],function (menuView) {
 
     
     
-    // Recupere os valores de entrada dos elementos HTML
+    
     const amount = document.getElementById('amount').value;
     const category = document.getElementById('theme').value;
     const difficultyInput = document.querySelector('input[name="difficulty"]:checked');
     const difficulty = difficultyInput ? difficultyInput.value : null;
     const type = "multiple";
 
-    // Valide os valores de entrada ou forneça padrões
+    // Valida os valores de entrada ou forneça padrões
     if (!amount || !category || !difficulty) {
       console.error("Valores de entrada inválidos");
       return null;
     }
 
-    // Construa a URL da API
+    // Construir a URL da API
     const url = "https://opentdb.com/api.php?amount=" + amount + "&category=" + category + "&difficulty=" + difficulty + "&type=" + type;
     const options = {
       method: "GET",
     };
 
     try {
-      // Busque e analise a resposta JSON
+      
       return await (await fetch(url, options)).json();
     } catch (error) {
       console.error(error);
